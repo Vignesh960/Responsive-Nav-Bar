fb10c6318243427db8d1d79589b115d5bc4afb7a
vignesh chari <vigneshvangala@gmail.com>
2022-12-12 21:02:31 +0530

    completed nv

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,39 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  const[isMobile,setIsMobile]=useState(true);
+  const [isMobile, setIsMobile] = useState(false);
   return (
-   <header className='brand_header'>
-    <nav className={isMobile?'brand_header_links_mobile':"brand_header_links"} onClick={(e)=>setIsMobile(false)}>
-      <NavLink className="brand_header_links_items" to="/">Home</NavLink>
-      <NavLink className="brand_header_links_items" to="/about">About</NavLink>
-      <NavLink className="brand_header_links_items" to="">Contact</NavLink>
-      <NavLink className="brand_header_links_items" to="">Signup</NavLink>
+    <header className="brand_header">
+      <nav
+        className={
+          isMobile ? "brand_header_links_mobile" : "brand_header_links"
+        }
+        onClick={(e) => setIsMobile(false)}
+      >
+        <NavLink className="brand_header_links_items" to="/">
+          Home
+        </NavLink>
+        <NavLink className="brand_header_links_items" to="/about">
+          About
+        </NavLink>
+        <NavLink className="brand_header_links_items" to="">
+          Contact
+        </NavLink>
+        <NavLink className="brand_header_links_items" to="">
+          Signup
+        </NavLink>
+      </nav>
+      <button className="mobile-menu" onClick={()=>setIsMobile(!isMobile)}>
+        
+        {isMobile ? (
+          <i className="fa fa-times" aria-hidden="true"></i>
+        ) : (
+          <i className="fa fa-bars" aria-hidden="true"></i>
+        )}
+      </button>
+    </header>
+  );
+};
 
-    </nav>
-    <button className='mobile-menu'>{isMobile?<i className="fa fa-times" aria-hidden="true" ></i>:<i className="fa fa-bars" aria-hidden="true"></i>}</button>
-
-   </header>
-    
-  )
-}
-
-export default Header
+export default Header;
