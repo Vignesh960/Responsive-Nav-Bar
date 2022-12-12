ed138956387b35c1a6410c18bedf6610be9f8d65
vignesh chari <vigneshvangala@gmail.com>
2022-12-12 21:07:57 +0530

    mm

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,11 +5,12 @@ const Header = () => {
   const [isMobile, setIsMobile] = useState(false);
   return (
     <header className="brand_header">
+      <p className="logo">Logo</p>
       <nav
         className={
           isMobile ? "brand_header_links_mobile" : "brand_header_links"
         }
-        onClick={(e) => setIsMobile(false)}
+        onClick={() => setIsMobile(false)}
       >
         <NavLink className="brand_header_links_items" to="/">
           Home
@@ -17,15 +18,14 @@ const Header = () => {
         <NavLink className="brand_header_links_items" to="/about">
           About
         </NavLink>
-        <NavLink className="brand_header_links_items" to="">
+        <NavLink className="brand_header_links_items" to="/contact">
           Contact
         </NavLink>
-        <NavLink className="brand_header_links_items" to="">
+        <NavLink className="brand_header_links_items" to="/signup">
           Signup
         </NavLink>
       </nav>
-      <button className="mobile-menu" onClick={()=>setIsMobile(!isMobile)}>
-        
+      <button className="mobile-menu" onClick={() => setIsMobile(!isMobile)}>
         {isMobile ? (
           <i className="fa fa-times" aria-hidden="true"></i>
         ) : (
